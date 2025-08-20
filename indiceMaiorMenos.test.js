880ab70a00a068962bad689ae71ca2789cab5b8d
Eduferr <eduferr2803@gmail.com>
2025-08-19 23:55:03 -0300

    correção

--- indiceMaiorMenos.test.js
+++
@@ -1,7 +0,0 @@
-const { indiceMaiorMenor } = require('./indiceMaiorMenos')
-
-describe('Funcionalidade buscar Índice', () => {
-    it('Buscar os índices de maior e menor valor', () => {
-        expect(indiceMaiorMenor([5, 12, 3, 9, 1])).toEqual({ indiceMaior: 1, indiceMenor: 4 })
-    });
-});
